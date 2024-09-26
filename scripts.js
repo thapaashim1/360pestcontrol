808a2d0327eebbd9465914facce9d6ad6c4f277d
thapaashim1 <thapaashim1@gmail.com>
2024-09-27 09:24:43 +1000

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,6 @@
+// JavaScript for future interactivity
+
+// Example: Show alert when 'Get a Free Quote' button is clicked
+document.querySelector('.cta-button').addEventListener('click', function () {
+    alert('Thank you for your interest! We will get back to you shortly.');
+});
